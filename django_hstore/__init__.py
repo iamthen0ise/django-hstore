4878b5ee4ecbe8de6226018978ac109cfa3465c1
iamthen0ise <evgenii.uvarov.post@gmail.com>
2024-08-06 11:16:46 +0300

    Update __init__.py

--- django_hstore/__init__.py
+++ django_hstore/__init__.py
@@ -1,17 +1,9 @@
-VERSION = (1, 5, 0, 'alpha')
+VERSION = (2, 0, 0)
 __version__ = VERSION
 
 
 def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        if VERSION[3] != 'final':
-            version = '%s %s' % (version, VERSION[3])
-    return version
+    return ".".join(VERSION)
 
 
 default_app_config = 'django_hstore.apps.HStoreConfig'
